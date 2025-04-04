c3d7320c33208dd0649058992c93f962679b1459
erfan11k <erfankarimi737@gmail.com>
2025-04-04 10:16:20 +0330

    faqComplete

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,38 +1,42 @@
-import React from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
-import { MdOutlineLibraryBooks } from 'react-icons/md'
-import {questions} from './data'
+import React, {useEffect} from 'react';
+import { MdOutlineLibraryBooks } from "react-icons/md";
+import { questions } from './data.js';
+import './faq.css';
+import Question from './Question.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-const Faq = () => {
+function Faq() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000
+    });
+  }, [])
+
   return (
-  <section id='faq'>
-    <div className='faq-contanier'>
-      <div className='faq-title'>
-        <MdOutlineLibraryBooks color='orangered' size={30}/>
-        <h2>سوالات متداول</h2>
-        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیکی است</p>
-      </div>
+    <section id="faq">
+      <div className="container faq">
+        <div className="faq-title"  data-aos="fade-up">
+          <MdOutlineLibraryBooks color="orangered" size={30} />
+          <h2>سوالات متداول</h2>
+          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+        </div>
 
-      <div className='questions'>
-        {
-          questions && questions.map((question) =>
-            <div className='question' key={question.id}>
-              <div className='question-title'>
-                <h4>{question.title}</h4>
-                <button>
-                  <AiOutlinePlus/>
-                </button>
-              </div>
-              <div className='question-answer'>
-                <p>{question.answer}</p>
-              </div>
-            </div>
-          )
-        }
+        <div className="questions">
+          {
+            questions && questions.map((question) =>
+             <Question
+              key={question.id}
+              title={question.title}
+              answer={question.answer}
+             />
+            )
+          }
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Faq
+export default Faq;
